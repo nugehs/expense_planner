1479a6a05a6b29f859c6d7dbb587402255ed10b8
Segun Olumbe <david.olumbe@gmail.com>
2021-06-02 09:37:35 +0100

    map transaction to widget

--- lib/main.dart
+++ lib/main.dart
@@ -49,10 +49,13 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Card(
-            color: Colors.red,
-            child: Text('LIST OF TX'),
-            elevation: 5,
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                child: Text(tx.title),
+                elevation: 5,
+              );
+            }).toList(),
           ),
         ],
       ),
