3565b4c9b6dd26ed3aa892d018cba877ba91b4fb
Segun Olumbe <david.olumbe@gmail.com>
2021-06-07 07:12:13 +0100

    split transaction item

---
+++ lib/widgets/transaction_item.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../models/transaction.dart';
+
+class TransactionItem extends StatelessWidget {
+  const TransactionItem({
+    Key key,
+    @required this.transaction,
+    @required this.mediaQuery,
+    @required this.deleteTx,
+  }) : super(key: key);
+
+  final Transaction transaction;
+  final MediaQueryData mediaQuery;
+  final Function deleteTx;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      margin: const EdgeInsets.symmetric(
+        vertical: 8,
+        horizontal: 5,
+      ),
+      child: ListTile(
+        leading: CircleAvatar(
+          radius: 30,
+          child: Padding(
+            padding: EdgeInsets.all(6),
+            child: FittedBox(
+              child: Text('\$${transaction.amount}'),
+            ),
+          ),
+        ),
+        title: Text(
+          transaction[index].title,
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        subtitle: Text(
+          DateFormat.yMMMd().format(transaction.date),
+        ),
+        trailing: mediaQuery.size.width > 360
+            ? FlatButton.icon(
+                onPressed: () => deleteTx(transaction.id),
+                icon: const Icon(Icons.delete),
+                textColor: Theme.of(context).errorColor,
+                label: const Text('delete'))
+            : IconButton(
+                icon: const Icon(Icons.delete),
+                color: Theme.of(context).errorColor,
+                onPressed: () => deleteTx(transaction.id),
+              ),
+      ),
+    );
+  }
+}
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+
 import '../models/transaction.dart';
-import 'package:intl/intl.dart';
+import './transaction_item.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
@@ -35,42 +36,10 @@ class TransactionList extends StatelessWidget {
             })
           : ListView.builder(
               itemBuilder: (ctx, index) {
-                return Card(
-                  elevation: 5,
-                  margin: const EdgeInsets.symmetric(
-                    vertical: 8,
-                    horizontal: 5,
-                  ),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Padding(
-                        padding: EdgeInsets.all(6),
-                        child: FittedBox(
-                          child: Text('\$${transactions[index].amount}'),
-                        ),
-                      ),
-                    ),
-                    title: Text(
-                      transactions[index].title,
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                    subtitle: Text(
-                      DateFormat.yMMMd().format(transactions[index].date),
-                    ),
-                    trailing: mediaQuery.size.width > 360
-                        ? FlatButton.icon(
-                            onPressed: () => deleteTx(transactions[index].id),
-                            icon: const Icon(Icons.delete),
-                            textColor: Theme.of(context).errorColor,
-                            label: const Text('delete'))
-                        : IconButton(
-                            icon: const Icon(Icons.delete),
-                            color: Theme.of(context).errorColor,
-                            onPressed: () => deleteTx(transactions[index].id),
-                          ),
-                  ),
-                );
+                return TransactionItem(
+                    transaction: transactions[index],
+                    mediaQuery: mediaQuery,
+                    deleteTx: deleteTx);
               },
               itemCount: transactions.length,
             ),
